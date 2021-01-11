600e5aa9b2ac4bd9bb999ff095467d37ac52d943
sanguo <573519246@qq.com>
2021-01-11 13:53:04 +0800

    线程插队

---
+++ src/main/java/thread/TestJoin.java
@@ -0,0 +1,35 @@
+package thread;
+
+/**
+ * @program: Juc
+ * @description: join插队
+ * @author: Saxon
+ * @create: 2021-01-08 15:47
+ */
+public class TestJoin implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("join 线程 " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        TestJoin testJoin = new TestJoin();
+        Thread thread = new Thread(testJoin);
+        thread.start();
+
+        for (int i = 0; i < 500; i++) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("main " + i);
+
+        }
+    }
+}
+
+
