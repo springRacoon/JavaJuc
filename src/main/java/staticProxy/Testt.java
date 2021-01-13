7da7530a6ebfb291b0df150a674c0d8a9b43925d
sanguo <573519246@qq.com>
2021-01-14 00:32:25 +0800

    线程池的基础使用

---
+++ src/main/java/staticProxy/Testt.java
@@ -0,0 +1,30 @@
+package staticProxy;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @program: Juc
+ * @description:
+ * @author: Saxon
+ * @create: 2021-01-13 08:58
+ */
+public class Testt {
+
+    public static void main(String[] args) {
+
+        Set<Integer> set = new HashSet<>();
+
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+
+        set.remove(2);
+
+        for (Integer integer : set) {
+            System.out.println(integer);
+        }
+
+    }
+}
