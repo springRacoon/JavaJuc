a25cf6b40dcbc770079dcd1dd1a3006ef0fcdd2a
sanguo <573519246@qq.com>
2021-01-15 10:35:23 +0800

    虚假唤醒，Condition，Lock和synchronized

---
+++ src/main/java/juc/pc/C.java
@@ -0,0 +1,97 @@
+package juc.pc;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @program: Juc
+ * @description:
+ * @author: Saxon
+ * @create: 2021-01-14 16:03
+ */
+public class C {
+
+    public static void main(String[] args) {
+
+        Data3 data = new Data3();
+
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                data.printA();
+            }
+        }, "A").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                data.printB();
+            }
+        }, "B").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                data.printC();
+            }
+        }, "C").start();
+
+
+    }
+}
+
+class Data3 {
+
+    private Lock lock = new ReentrantLock();
+    private Condition condition1 = lock.newCondition();
+    private Condition condition2 = lock.newCondition();
+    private Condition condition3 = lock.newCondition();
+    private int  number = 1;
+
+
+
+    public void printA() {
+        lock.lock();
+        try {
+            while (number!=1){
+                condition1.await();
+            }
+            System.out.println(Thread.currentThread().getName()+"AAAA");
+            number =2;
+            condition2.signal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+
+    }
+
+    public void printB() {
+        lock.lock();
+        try {
+            while (number!=2){
+                condition2.await();
+            }
+            System.out.println(Thread.currentThread().getName()+"BBBB");
+            number =3;
+            condition3.signal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void printC() {
+        lock.lock();
+        try {
+            while (number!=3){
+                condition3.await();
+            }
+            System.out.println(Thread.currentThread().getName()+"CCCC");
+            number =1;
+            condition1.signal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
