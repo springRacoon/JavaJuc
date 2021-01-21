be06d0279ef2b4d4184d874f8d98484833fcbd96
sanguo <573519246@qq.com>
2021-01-21 17:11:13 +0800

    函数式接口

--- src/main/java/juc/function/SupplierDemo.java
+++ src/main/java/juc/function/SupplierDemo.java
@@ -12,7 +12,7 @@ public class SupplierDemo {
 
     public static void main(String[] args) {
 
-        Supplier supplier = new  <String>() {
+        Supplier supplier = new Supplier<String>() {
             @Override
             public String get() {
                 return "123";
