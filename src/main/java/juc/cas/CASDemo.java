1307071e42b9b3e717fb97cd9d5029dfe6e89c74
sanguo <573519246@qq.com>
2021-01-26 16:19:17 +0800

    ABA问题

--- src/main/java/juc/cas/CASDemo.java
+++ src/main/java/juc/cas/CASDemo.java
@@ -1,6 +1,9 @@
 package juc.cas;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * @program: Juc
@@ -13,8 +16,34 @@ public class CASDemo {
 
     public static void main(String[] args) {
 
-        AtomicInteger atomicInteger = new AtomicInteger(2020);
-        atomicInteger.compareAndSet(2020,2021);
-        System.out.println(atomicInteger.get());
+//        AtomicInteger atomicInteger = new AtomicInteger(2020);
+//        atomicInteger.compareAndSet(2020,2021);
+//        System.out.println(atomicInteger.get());
+
+        AtomicStampedReference<Integer> atomicStampedReference = new AtomicStampedReference<>(111,1);
+
+        new Thread(()->{
+            System.out.println(atomicStampedReference.getStamp());
+            atomicStampedReference.compareAndSet(111,123,atomicStampedReference.getStamp(),atomicStampedReference.getStamp()+1);
+            System.out.println(atomicStampedReference.getStamp());
+            atomicStampedReference.compareAndSet(123,111,atomicStampedReference.getStamp(),atomicStampedReference.getStamp()+1);
+            System.out.println(atomicStampedReference.getStamp());
+
+
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        },"a").start();
+
+        new Thread(()->{
+            System.out.println(atomicStampedReference.getStamp());
+            atomicStampedReference.compareAndSet(111,123,atomicStampedReference.getStamp(),atomicStampedReference.getStamp()+1);
+            System.out.println(atomicStampedReference.getStamp());
+
+        },"b").start();
+
     }
 }
